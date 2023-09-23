4693c80929ed5cb6f24df601ed01967081cc82a3
Shubham <convey.shubham@gmail.com>
2023-09-23 15:29:13 +0530

    refactored code: moved functions into helpers.js

--- index.js
+++ index.js
@@ -1,44 +1,11 @@
 const readline = require("readline");
 const fs = require("fs");
 const { table } = require("table");
+const { wrapText, formatDate } = require("./helpers");
 const statusCodes = require("./statusCodes.json");
+
+//  read the filePath from command line options
 const [filePath] = process.argv.slice(2);
-
-/*
- * formats date as a string in YYYY-MMMM-DD HH:MM
- * {string} dateString
- * {string}
- */
-const formatDate = (dateString) => {
-  const date = new Date(dateString);
-
-  const year = date.getUTCFullYear();
-  const month = date.getUTCMonth() + 1;
-  const day = date.getUTCDate();
-
-  // Get the time components
-  const hours = date.getUTCHours();
-  const minutes = date.getUTCMinutes();
-
-  const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}`;
-
-  return formattedDate;
-};
-
-/*
- *   enables string to wrap over multiple lines if it exceeds maxLength
- *   {string} text
- *   {maxLength} number of characters after which text should wrap
- */
-const wrapText = (text, maxLength = 50) => {
-  let formattedText = "";
-  let index = 0;
-  while (index < text.length) {
-    formattedText += text.substring(index, index + maxLength) + "\n";
-    index += maxLength;
-  }
-  return formattedText;
-};
 
 // object to store the Overall API stats
 const insights = {
@@ -92,6 +59,7 @@ const extractInformationFromLine = (line = "") => {
     ? insights.totalReqsPerMinute[formattedDate] + 1
     : 1;
 };
+
 /*
  * processes log file and generates the stastics
  * {String} filePath
